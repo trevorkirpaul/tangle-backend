106381d7c1d1864a77f295e9e198c403ac011a1c
Trevor Kirpaul <tkirpaul@gmail.com>
2018-01-19 01:40:52 -0500

    added post delete route, handler

--- controllers/Posts.js
+++ controllers/Posts.js
@@ -1,13 +1,29 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-// todo: replace userID with decoding JWT for userID
+const jwt = require('../services/jwt');
+const { readToken } = jwt;
+
 exports.create = (req, res, next) => {
-  const userID = req.body.userID;
+  const token = readToken(req.body.token);
+  const userID = token.userID;
+
   const post = { ...req.body.post, author: userID };
 
   Post.create(post)
     .then(post =>
       User.findByIdAndUpdate(userID, { $push: { posts: post } }, { new: true })
     )
-    .then(user => res.send({ user }));
+    .then(user => res.send({ user }))
+    .catch(next);
+};
+
+exports.delete = (req, res, next) => {
+  const token = readToken(req.body.token);
+  const userID = token.userID;
+  const postID = req.body.postID;
+
+  User.findByIdAndUpdate(userID, { $pull: { posts: postID } }, { new: true })
+    .then(() => Post.findByIdAndRemove(postID))
+    .then(post => res.send({ postRemoved: true, post }))
+    .catch(next);
 };
--- routes/routes.js
+++ routes/routes.js
@@ -5,11 +5,16 @@ module.exports = app => {
   app.get('/', (req, res, next) => {
     res.send({ message: 'Welcome to the backend server for Tangle' });
   });
-  // user
+  // create user
   app.post('/user', UserController.create);
+  // auto sign in
   app.post('/token-signin', UserController.tokenSignIn);
+  // delete user
   app.delete('/user', UserController.delete);
+  // update user
   app.put('/user', UserController.update);
-  // posts
+  // create posts
   app.post('/post', PostsController.create);
+  // delete post
+  app.delete('/post', PostsController.delete);
 };
